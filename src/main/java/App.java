dd4511eacef7ece6078c22ba0b54f555c3e364bf
Гамзатов Ибрагим <ibr.gamzatov.090905@mail.ru>
2024-12-23 21:01:32 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+
+        Cat bean_cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_cat.getName());
+
+        Cat bean_cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_cat2.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(bean_cat == bean_cat2);
     }
 }
